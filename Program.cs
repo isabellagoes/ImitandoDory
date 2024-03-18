e6206d82dd83b98f272025b72f86ecdf289f3b12
isabellagoes <141466024+isabellagoes@users.noreply.github.com>
2024-03-18 19:42:32 +0000

    conclusão: tenho que revisar c#

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("---------------- Imitando Dory ----------------\n");
+
+Console.Write("Digite uma frase para ser traduzida em baleiês: ");
+string frase = Console.ReadLine()!;
+
+string tradBaleies = frase
+    .Replace("a", "aaa")
+    .Replace("A", "AAA")
+    .Replace("e", "eee")
+    .Replace("E", "EEE")
+    .Replace("i", "iii")
+    .Replace("I", "III")
+    .Replace("o", "ooo")
+    .Replace("O", "OOO")
+    .Replace("u", "uuu")
+    .Replace("U", "UUU")
+;
+
+Console.WriteLine($"\nEm baleiês: {tradBaleies}");
